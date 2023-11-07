2d190a5279c465102c08c6234d21e1af90cddd4e
Dina Pavlyukovich <109734500+dinapavlyukovich@users.noreply.github.com>
2023-11-07 17:08:47 +0300

    внесены корректироки в js файлы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
 
+const sliderBrands = document.querySelector('.swiper-container-1')
+
+const swiper1 = new Swiper(sliderBrands, {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+        breakpoints: {
+        300: {
+            slidesPerView: 'auto',
+            spaceBetween: -50,
+        },
+        767: {
+            enabled: false,
+            onAny() {
+                swiper1.destroy(true, true);
+            }
+        }
+
+    }
+});  
+
+const sliderBrands2 = document.querySelector('.swiper-container-2')
+
+const swiper2 = new Swiper(sliderBrands2, {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+        breakpoints: {
+        300: {
+            slidesPerView: 'auto',
+            spaceBetween: -50,
+        },
+        767: {
+            enabled: false,
+            onAny() {
+                swiper2.destroy(true, true);
+            }
+        }
+ 
+    }
+ }); 
+
+ const swiper3 = new Swiper('.swiper-container-3', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+        breakpoints: {
+        300: {
+            slidesPerView: 'auto',
+            spaceBetween: -50,
+            
+        },
+        767: {
+            enabled: false,
+            onAny() {
+                swiper3.destroy(true, true);
+            }
+        }
+ 
+    }
+ }); 
